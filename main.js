856b7dc28ec1eef071993d4799833b7dde2dc347
Anastasiia4435 <107216310+Anastasiia4435@users.noreply.github.com>
2023-03-02 19:25:09 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+let numbers = [1, 5, 4, 8, 10, -5, 0, 9];
+console.log(numbers);
+
+let names = ["Nastya", "Masha", "Vika", "Kirill", "Lena", "Evgenij", "Veronika", "Mark", "Ludmila", "Valera"];
+console.log(names[0]);
+console.log(names[3]);
+console.log(names[7]);
+
+let nothing = [];
+nothing[0] = 10;
+nothing[1] = 8;
+nothing[4] = 100;
+console.log(nothing);
